e1b134f74edb4d17af49fe2f5e0de3c99a4803dd
rwaeng <rwaeng0421@gmail.com>
2024-08-08 17:55:23 +0900

    💚 예시 컴포넌트 작성

---
+++ src/components/common/Example.style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { flexCenter } from '../../styles/common';
+import { bold56 } from '../../styles/font';
+
+const S = {};
+
+S.Wrapper = styled.span`
+  ${flexCenter}
+  ${bold56}
+  color: var(--white);
+`;
+
+export { S };
