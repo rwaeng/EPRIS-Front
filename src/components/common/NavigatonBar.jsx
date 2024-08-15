9f1710ab09ebf562653a6bd85a34566bb3d7f479
rwaeng <rwaeng0421@gmail.com>
2024-08-15 20:49:35 +0900

    🔧 네비바 주소 수정 및 admin 네비바 추가

--- src/components/common/NavigatonBar.jsx
+++ src/components/common/NavigatonBar.jsx
@@ -1,58 +1,75 @@
-import { useState } from 'react';
 import { S, M } from './NavigatonBar.style';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-const NavigationBar = () => {
+
+const NavigationBar = ({ type }) => {
   const navigate = useNavigate();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const clickMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
   return (
     <S.Layout>
-      <S.Container>
-        <S.LogoImg onClick={() => navigate('/')} />
-        <S.NavContainer>
-          <S.NavLinkWrapper to='/' activeClassName='active'>
-            About
-          </S.NavLinkWrapper>
-          <S.NavLinkWrapper to='/eprians' activeClassName='active'>
-            EPRians
-          </S.NavLinkWrapper>
-          <S.NavLinkWrapper to='/activities' activeClassName='active'>
-            Activities
-          </S.NavLinkWrapper>
-          <S.NavLinkWrapper to='/join' activeClassName='active'>
-            Join us
-          </S.NavLinkWrapper>
-          <S.NavLinkWrapper to='/recruitment' activeClassName='active'>
-            Recruitment
-          </S.NavLinkWrapper>
-          <S.NavLinkWrapper to='/contact' activeClassName='active'>
-            Contact
-          </S.NavLinkWrapper>
-        </S.NavContainer>
-        <M.NavMenu $isMenuOpen={isMenuOpen} onClick={clickMenu} />
-      </S.Container>
+      {type === 'admin' ? (
+        <S.Container>
+          <S.LogoImg onClick={() => navigate('/')} />
+          <S.NavContainer>
+            <S.NavLink to='/admin/info' activeClassName='active'>
+              기본정보
+            </S.NavLink>
+            <S.NavLink to='/admin/eprians' activeClassName='active'>
+              EPRians
+            </S.NavLink>
+            <S.NavLink to='/admin/activities' activeClassName='active'>
+              Activities
+            </S.NavLink>
+            <S.NavLink to='/admin/recruitment' activeClassName='active'>
+              Recruitment
+            </S.NavLink>
+          </S.NavContainer>
+        </S.Container>
+      ) : (
+        <S.Container>
+          <S.LogoImg onClick={() => navigate('/')} />
+          <S.NavContainer>
+            <S.NavLink to='/about' activeClassName='active'>
+              About
+            </S.NavLink>
+            <S.NavLink to='/eprians' activeClassName='active'>
+              EPRians
+            </S.NavLink>
+            <S.NavLink to='/activities' activeClassName='active'>
+              Activities
+            </S.NavLink>
+            <S.NavLink to='/joinus' activeClassName='active'>
+              Join us
+            </S.NavLink>
+            <S.NavLink to='/contact' activeClassName='active'>
+              Contact
+            </S.NavLink>
+          </S.NavContainer>
+          <M.NavMenu $isMenuOpen={isMenuOpen} onClick={clickMenu} />
+        </S.Container>
+      )}
       {isMenuOpen && (
         <M.NavContainer>
-          <S.NavLinkWrapper to='/' activeClassName='active'>
+          <S.NavLink to='/about' activeClassName='active'>
             About
-          </S.NavLinkWrapper>
-          <S.NavLinkWrapper to='/eprians' activeClassName='active'>
+          </S.NavLink>
+          <S.NavLink to='/eprians' activeClassName='active'>
             EPRians
-          </S.NavLinkWrapper>
-          <S.NavLinkWrapper to='/activities' activeClassName='active'>
+          </S.NavLink>
+          <S.NavLink to='/activities' activeClassName='active'>
             Activities
-          </S.NavLinkWrapper>
-          <S.NavLinkWrapper to='/join' activeClassName='active'>
+          </S.NavLink>
+          <S.NavLink to='/joinus' activeClassName='active'>
             Join us
-          </S.NavLinkWrapper>
-          <S.NavLinkWrapper to='/recruitment' activeClassName='active'>
-            Recruitment
-          </S.NavLinkWrapper>
-          <S.NavLinkWrapper to='/contact' activeClassName='active'>
+          </S.NavLink>
+          <S.NavLink to='/contact' activeClassName='active'>
             Contact
-          </S.NavLinkWrapper>
+          </S.NavLink>
         </M.NavContainer>
       )}
     </S.Layout>
