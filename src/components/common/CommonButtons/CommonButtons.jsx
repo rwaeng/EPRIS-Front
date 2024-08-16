a6c233a7bd0064571d1d5646613af829ca46cd7b
m-oooooonchild <syp614@ewhain.net>
2024-08-17 04:58:57 +0900

    🐛 : 공통버튼 onClick 핸들러 추가

--- src/components/common/CommonButtons/CommonButtons.jsx
+++ src/components/common/CommonButtons/CommonButtons.jsx
@@ -6,27 +6,43 @@ export const TextIconButton = ({
   bgColor,
   border,
   color,
+  handlebutton,
   ...props
 }) => {
   // text : 버튼에 들어갈 문구를 텍스트로 전달
   // icon : 버튼에 들어갈 아이콘의 소스 경로를 전달
   // bgColor : 버튼의 배경색을 텍스트로 전달
   // border : 버튼의 border를 텍스트로 전달
+  // handleButton : 버튼을 클릭했을 때 실행되는 함수 전달
 
   return (
-    <S.JoinUsContainer $bgColor={bgColor} $border={border} {...props}>
+    <S.JoinUsContainer
+      onClick={handlebutton}
+      $bgColor={bgColor}
+      $border={border}
+      {...props}
+    >
       <S.Span $color={color}>{text}</S.Span>
       <S.Icon src={icon} />
     </S.JoinUsContainer>
   );
 };
 
-export const TextButton = ({ isActive, text, ...props }) => {
+export const TextButton = ({ isActive, text, handlebutton, ...props }) => {
   // text : 버튼에 들어갈 문구를 텍스트로 전달
   // inActive : 버튼의 활성화 여부를 boolean으로 전달
+  // handleButton : 버튼을 클릭했을 때 실행되는 함수 전달
 
   return (
-    <S.UpdateContainer isActive={isActive} {...props}>
+    <S.UpdateContainer
+      isActive={isActive}
+      onClick={() => {
+        if (isActive) {
+          handlebutton();
+        }
+      }}
+      {...props}
+    >
       <S.Text>{text}</S.Text>
     </S.UpdateContainer>
   );
