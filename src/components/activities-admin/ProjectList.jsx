1f47d1b46f2de604600e045ada5d0a9b08270373
rwaeng <rwaeng0421@gmail.com>
2024-08-25 22:35:19 +0900

    ✨ admin 액티비티 페이지 api 연결

--- src/components/activities-admin/ProjectList.jsx
+++ src/components/activities-admin/ProjectList.jsx
@@ -1,53 +1,84 @@
 import { S } from './AdminActivityItem.style';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { deleteProject, getProject } from '../../api/project';
 import { TextIconButton } from '../common/CommonButtons/CommonButtons';
 import ProjectItem from './ProjectItem';
 import Plus from '../../assets/admin_activity/plus.svg';
 
 const ProjectList = () => {
-  const [projectList, setProjectList] = useState([
-    {
-      projectId: 1,
-      year: '~2022',
-      info: '00캠페인 기획',
-    },
-    {
-      projectId: 2,
-      year: '2023',
-      info: '00캠페인 기획',
-    },
-    {
-      projectId: 3,
-      year: '2024',
-      info: '00캠페인 기획',
-    },
-  ]);
+  const sessionStorageList = sessionStorage.getItem('list')
+    ? JSON.parse(sessionStorage.getItem('list'))
+    : [];
+  const [projectList, setProjectList] = useState([]);
+  const [maxYear, setMaxYear] = useState(null);
 
   // 프로젝트 묶음 삭제
   const handleDeleteItem = e => {
+    const { id } = e.target;
+
     // id 확인 후 new이면 그냥 삭제
     // 아니면 delete 요청
-    const newList = projectList.filter(
-      (_, idx) => parseInt(e.target.id) !== idx,
-    );
-    setProjectList(newList);
+    if (id && projectList[id].projectId === 'new') {
+      const newList = projectList.filter(
+        (it, idx) => it.projectId === 'new' && parseInt(id) !== idx,
+      );
+      sessionStorage.setItem('list', JSON.stringify(newList));
+    } else {
+      deleteProjectItem(Number(projectList[id].projectId));
+    }
+  };
+
+  // 프로젝트 아이템 delete
+  const deleteProjectItem = async projectId => {
+    try {
+      const res = await deleteProject(projectId);
+      if (res) {
+        alert('삭제되었습니다.');
+        const newList = projectList.filter(it => it.projectId === 'new');
+        sessionStorage.setItem('list', JSON.stringify(newList));
+      }
+    } catch (e) {
+      alert('삭제하는 동안 오류가 발생했습니다. 다시 시도해주세요.');
+      console.error(e);
+    }
   };
 
   // 프로젝트 묶음 추가
   const handleClickButton = () => {
-    setProjectList([...projectList, { id: 'new', year: '', info: '' }]);
+    setProjectList([...projectList, { projectId: 'new', year: '', info: '' }]);
   };
+
+  // 프로젝트 리스트 read
+  const readProject = async () => {
+    try {
+      const res = await getProject();
+      const sortedList = res.sort((a, b) => a.year - b.year);
+      setProjectList([...sortedList, ...sessionStorageList]);
+      setMaxYear(sortedList[sortedList.length - 1].year);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    readProject();
+  }, []);
 
   return (
     <S.Layout>
-      {projectList.map((it, idx) => (
-        <ProjectItem
-          key={idx}
-          it={it}
-          num={idx + 1}
-          onClick={handleDeleteItem}
-        />
-      ))}
+      {projectList.length > 0 &&
+        projectList.map((it, idx) => (
+          <ProjectItem
+            key={idx}
+            it={it}
+            num={idx + 1}
+            projectList={projectList}
+            setProjectList={setProjectList}
+            length={projectList.length}
+            onClick={handleDeleteItem}
+            maxYear={maxYear}
+          />
+        ))}
       <TextIconButton
         text='프로젝트 묶음 추가하기'
         icon={Plus}
