6854ce27fbe896302edaf87d692b8f2f2b0714bf
rwaeng <rwaeng0421@gmail.com>
2024-08-25 22:28:15 +0900

    ✨ api 호출 함수 작성

---
+++ src/api/network.js
@@ -0,0 +1,29 @@
+import { authInstance, instance } from '.';
+
+export const getNetwork = async () => {
+  try {
+    const response = await instance.get(`/networks`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const postNetwork = async (type, networkInfo, imageUrl) => {
+  try {
+    const response = await authInstance.post(
+      `/networks`,
+      {
+        networkInfo: networkInfo,
+        imageUrl: imageUrl,
+      },
+      {
+        params: { type: type },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.log('실패')
+    throw error;
+  }
+};
