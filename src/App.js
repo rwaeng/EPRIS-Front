a5c79a3c4cd9d9f079a98a667f40d33a344313f6
rwaeng <rwaeng0421@gmail.com>
2024-08-27 17:25:40 +0900

    ✨ 리액트 lazy 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
+import { Suspense, lazy } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
-
-import MainPage from './pages/Main/MainPage';
-import AboutPage from './pages/About/AboutPage';
-import ActivityPage from './pages/Activity/ActivityPage';
-import ContactPage from './pages/Contact/ContactPage';
-import EPRiansPage from './pages/EPRians/EPRiansPage';
-import JoinusPage from './pages/Joinus/JoinusPage';
 
 import LoginPage from './pages/Admin/LoginPage';
 import AdminActivityPage from './pages/Admin/ActivityPage';
@@ -13,31 +7,49 @@ import ClassInfoPage from './pages/Admin/ClassInfoPage';
 import AdminEPRiansPage from './pages/Admin/EPRiansPage';
 import RecruitmentPage from './pages/Admin/RecruitmentPage';
 
-import LayoutPage from './pages/LayoutPage';
 import PrivateRouter from './pages/PrivateRouter';
 
+const LayoutPage = lazy(() => import('./pages/LayoutPage'));
+const MainPage = lazy(() => import('./pages/Main/MainPage'));
+const AboutPage = lazy(() => import('./pages/About/AboutPage'));
+const ContactPage = lazy(() => import('./pages/Contact/ContactPage'));
+const EPRiansPage = lazy(() => import('./pages/EPRians/EPRiansPage'));
+const JoinusPage = lazy(() => import('./pages/Joinus/JoinusPage'));
+const ActivityPage = lazy(() => import('./pages/Activity/ActivityPage'));
+
 const App = () => {
+  const adminUrl = process.env.REACT_APP_ADMIN_URL;
+
   return (
-    <Routes>
-      <Route element={<LayoutPage />}>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/activities' element={<ActivityPage />} />
-        <Route path='/contact' element={<ContactPage />} />
-        <Route path='/eprians' element={<EPRiansPage />} />
-        <Route path='/joinus' element={<JoinusPage />} />
-      </Route>
+    <Suspense fallback={<></>}>
+      <Routes>
+        <Route element={<LayoutPage />}>
+          <Route path='/' element={<MainPage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/activities' element={<ActivityPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+          <Route path='/eprians' element={<EPRiansPage />} />
+          <Route path='/joinus' element={<JoinusPage />} />
+        </Route>
 
-      <Route path='/admin' element={<LoginPage />} />
-      <Route element={<PrivateRouter />}>
-        <Route path='/admin/info' element={<ClassInfoPage />} />
-        <Route path='/admin/eprians' element={<AdminEPRiansPage />} />
-        <Route path='/admin/activities' element={<AdminActivityPage />} />
-        <Route path='/admin/recruitment' element={<RecruitmentPage />} />
-      </Route>
+        {/* 관리자 페이지 */}
+        <Route path={adminUrl} element={<LoginPage />} />
+        <Route element={<PrivateRouter />}>
+          <Route path={`${adminUrl}/info`} element={<ClassInfoPage />} />
+          <Route path={`${adminUrl}/eprians`} element={<AdminEPRiansPage />} />
+          <Route
+            path={`${adminUrl}/activities`}
+            element={<AdminActivityPage />}
+          />
+          <Route
+            path={`${adminUrl}/recruitment`}
+            element={<RecruitmentPage />}
+          />
+        </Route>
 
-      <Route path='*' element={<Navigate to='/' />} />
-    </Routes>
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+    </Suspense>
   );
 };
 
