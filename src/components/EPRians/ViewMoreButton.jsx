a210b41569908a62dc9e48458aa8c4e46f7f38a4
m-oooooonchild <syp614@ewhain.net>
2024-08-28 21:56:03 +0900

    ♻️ 수정 사항 반영

--- src/components/EPRians/ViewMoreButton.jsx
+++ src/components/EPRians/ViewMoreButton.jsx
@@ -2,9 +2,9 @@ import { S } from './ViewMoreButton.style';
 import useScrollFadeIn from '../../hooks/useScrollFadeIn';
 
 const ViewMoreButton = ({ className, onClick, text, icon }) => {
-  const animation = useScrollFadeIn({ threshold: 0.7, initialOffset: '70%' });
+  const aniButton = useScrollFadeIn({ threshold: 0.7, initialOffset: '70%' });
   return (
-    <S.Container className={className} onClick={onClick} {...animation}>
+    <S.Container className={className} onClick={onClick} {...aniButton}>
       <S.Text>{text}</S.Text>
       <S.Icon src={icon} alt='icon' />
     </S.Container>
